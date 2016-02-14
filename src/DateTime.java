ad5de3eee2714eed45dd7e68d1f4d59152cfce5c
Annabel <Annabel>
2016-02-14 12:21:08 +0800

    Added timeInt attribute and method to call this attribute

--- src/DateTime.java
+++ src/DateTime.java
@@ -1,11 +1,12 @@
 
 public class DateTime {
 	String date;	// 08/02/2016
-	String time;		// 1300
+	String timeStr;		// 1300
 	String day;		// Mon
 	int dd;			// 8
 	int mm;			// Feb (Months will all be in short form for now)
 	int yy;			// 2016 	
+	int timeInt;
 	String[] months = new String[] {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 	String[] daysInWeek = new String[] {"Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"};
 	
@@ -13,12 +14,13 @@ public class DateTime {
 		this.dd = date[0];
 		this.mm = date[1];
 		this.yy = date[2];
-		this.time = convertTimeToString(time);
+		this.timeInt = time;
+		this.timeStr = convertTimeIntToString(time);
 		this.day = day;
 		this.date = this.dd + " " + months[this.mm - 1] + " " + this.yy;
 	}
 	
-	private String convertTimeToString(int time2) {
+	private String convertTimeIntToString(int time2) {
 		String temp;
 		if(time2 == 0) {
 			temp = "0000";
@@ -36,7 +38,7 @@ public class DateTime {
 		this.dd = date[0];
 		this.mm = date[1];
 		this.yy = date[2];
-		this.time = String.valueOf(-1);
+		this.timeStr = String.valueOf(-1);
 		this.day = day;
 		this.date = this.dd + " " + months[this.mm - 1] + " " + this.yy;
 	}
@@ -50,7 +52,7 @@ public class DateTime {
 		this.dd = date[0];
 		this.mm = date[1];
 		this.yy = date[2];
-		this.time = convertTimeToString(time2);
+		this.timeStr = convertTimeIntToString(time2);
 		this.day = day;
 		this.date = this.dd + " " + months[this.mm - 1] + " " + this.yy;
 	}
@@ -63,12 +65,28 @@ public class DateTime {
 		return this.date;
 	}
 	
-	public String getTime() {
-		return this.time;
+	public String getTimeStr() {
+		return this.timeStr;
+	}
+	
+	public int getTimeInt() {
+		return this.timeInt;
+	}
+	
+	public int getDD() {
+		return this.dd;
+	}
+	
+	public int getMM() {
+		return this.mm;
+	}
+	
+	public int getYY() {
+		return this.yy;
 	}
 	
 	public String toString() {
-		String ans = this.date + " " + this.day + " " + this.time;
+		String ans = this.date + " " + this.day + " " + this.timeStr;
 		return ans;
 	}
 }

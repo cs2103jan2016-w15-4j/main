b763abee510ee9f77e94e559f9d6c2243a7a980a
Annabel <Annabel>
2016-02-15 18:43:57 +0800

    Added in use of edit, delete and show parser objects

--- src/Parser.java
+++ src/Parser.java
@@ -32,9 +32,19 @@ public class Parser {
 			command = addParser.getCommand();
 			//command = CommandUtils.createAddCommand(data);
 			break;
+			
+		case Main.COMMAND_SHOW:
+			ShowParser showParser = new ShowParser(input);
+			command = showParser.getCommand();
+			
+		case Main.COMMAND_EDIT:
+			EditParser editParser = new EditParser(input);
+			command = editParser.getCommand();
 
 		case Main.COMMAND_DELETE:
-			command = CommandUtils.createDeleteCommand(data);
+			DeleteParser deleteParser = new DeleteParser(input);
+			command = deleteParser.getCommand();
+			//command = CommandUtils.createDeleteCommand(data);
 			break;
 
 		case Main.COMMAND_EXIT:

0073a0ace8646e2f61696472aa26fe31ef94e4ca
Annabel <Annabel>
2016-02-15 18:42:55 +0800

    Created DeleteParser class

---
+++ src/DeleteParser.java
@@ -0,0 +1,13 @@
+
+public class DeleteParser {
+
+	public DeleteParser(String input) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Command getCommand() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

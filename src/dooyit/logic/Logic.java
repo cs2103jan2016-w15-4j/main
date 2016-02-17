4b25e74ab941cfdad30c414ae36ca36c584a58c7
Dexter Mooi <mooicydexter@gmail.com>
2016-02-17 21:22:42 +0800

    Resolved merge conflicts for Main, Logic and Storage classes.

---
+++ src/dooyit/logic/Logic.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+
+public class Logic {
+
+	Parser parser;
+	TaskManager taskManager;
+	Storage storage;
+	
+	public Logic() throws IOException{
+		parser = new Parser();
+		taskManager = new TaskManager();
+		storage = new Storage();
+		storage.loadTasks(taskManager);
+	}
+	
+	
+	public void processCommand(String input) throws IOException{
+		Command command = parser.getCommand(input);
+		command.execute(taskManager);
+		save();
+		
+		//update UI - UI.update();
+		taskManager.display();
+	}
+	
+	private void save() throws IOException{
+		storage.saveTasks(taskManager.getTasks());
+	}
+	
+}

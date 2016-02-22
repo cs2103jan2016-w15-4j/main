fd8622029de802da5f9af513b4b1c0220f4b76bd
Annabel <annabel.tay17@gmail.com>
2016-02-23 05:59:31 +0800

    Add completed AddParser file

---
+++ src/dooyit/parser/AddCatParser.java
@@ -0,0 +1,36 @@
+package dooyit.parser;
+
+import dooyit.logic.commands.Command;
+import dooyit.logic.commands.CommandUtils;
+
+public class AddCatParser {
+	
+	private static final int START_INDEX_ARGS = 7;
+	private static final String MARKER = " by ";
+	
+	private static String userInput;
+	private static String catName;
+	private static String colour;
+	private static Command cmd;
+	
+	public AddCatParser(String input) {
+		//userInput ignore the word "addcat"
+		userInput = input.trim().toLowerCase().substring(START_INDEX_ARGS).trim();
+	}
+	
+	public Command getCommand() {
+		parse();
+		setCmd();
+		return cmd;
+	}
+
+	private void setCmd() {
+		//cmd = CommandUtils.createAddCatCommand(catName, colour);
+	}
+
+	private void parse() {
+		int lastOccurrenceOfBy = userInput.lastIndexOf(MARKER);
+		catName = userInput.substring(0, lastOccurrenceOfBy).trim();
+		colour = userInput.substring(lastOccurrenceOfBy).replace(MARKER, "");
+	}
+}

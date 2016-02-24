d1ee2a43ef4f156aed7ea93900890a8bba84eb82
Wenqi Wu <wenqiwu93@gmail.com>
2016-02-25 01:16:29 +0800

    Category buttons now toggle with the rest

--- src/dooyit/ui/UICategoryBoxContainer.java
+++ src/dooyit/ui/UICategoryBoxContainer.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import dooyit.logic.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
@@ -12,16 +13,17 @@ public class UICategoryBoxContainer {
 	private VBox categoryBoxContainerView;
 	private ArrayList<UICategoryBox> categoryBoxList;
 	private Logic logic;
+	private ToggleGroup mainViewToggleGroup;
 	
-	public UICategoryBoxContainer(ArrayList<Category> categoryList, Logic logic){
+	public UICategoryBoxContainer(ArrayList<Category> categoryList, Logic logic, ToggleGroup mainViewToggleGroup){
 		this.logic = logic;
-		
+		this.mainViewToggleGroup = mainViewToggleGroup;
 		this.categoryBoxContainerView = new VBox();
 		this.categoryBoxContainerView.setSpacing(6);
 		this.categoryBoxList = new ArrayList<UICategoryBox>();
 		
 		for (int i = 0; i < categoryList.size(); i++){
-			UICategoryBox categoryBox = new UICategoryBox(this, categoryList.get(i));
+			UICategoryBox categoryBox = new UICategoryBox(this, categoryList.get(i), this.mainViewToggleGroup);
 			this.categoryBoxList.add(categoryBox);
 			this.categoryBoxContainerView.getChildren().add(categoryBox.getView());
 		}
@@ -39,7 +41,7 @@ public class UICategoryBoxContainer {
 		this.categoryBoxContainerView.getChildren().clear();
 		this.categoryBoxList.clear();
 		for (int i = 0; i < categoryList.size(); i++){
-			UICategoryBox categoryBox = new UICategoryBox(this, categoryList.get(i));
+			UICategoryBox categoryBox = new UICategoryBox(this, categoryList.get(i), this.mainViewToggleGroup);
 			this.categoryBoxList.add(categoryBox);
 			this.categoryBoxContainerView.getChildren().add(categoryBox.getView());
 		}

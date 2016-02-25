9de057f1ccbccdbc72d2a435edc96d63c5fd8aa1
Dexter Mooi <mooicydexter@gmail.com>
2016-02-25 09:18:11 +0800

    Fixed error in event format.

--- src/dooyit/storage/StorageOperations.java
+++ src/dooyit/storage/StorageOperations.java
@@ -4,7 +4,7 @@ public class StorageOperations {
 
 	static final String STORAGE_FLOAT_FORMAT = "%1$s";
 	static final String STORAGE_DEADLINE_FORMAT = "%1$s, %2$s, %3$s, %4$s, %5$s, %6$s";
-	static final String STORAGE_EVENT_FORMAT = "%1$s, %2$s, %3$s, %4$s, %5$s, %6$s, %7$s, %8$s, %9$s, %10$s, 11$s";
+	static final String STORAGE_EVENT_FORMAT = "%1$s, %2$s, %3$s, %4$s, %5$s, %6$s, %7$s, %8$s, %9$s, %10$s, %11$s";
 	
 	static final String NAME_FILE_STORAGE = "tasks.txt";
 

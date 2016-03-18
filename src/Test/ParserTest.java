28a122eff1b2eff4b5f48af08e99380970004a7e
Annabel Tay <annabel.tay17@gmail.com>
2016-03-18 21:41:01 +0800

    configuring power mock

--- src/Test/ParserTest.java
+++ src/Test/ParserTest.java
@@ -2,8 +2,15 @@ package Test;
 
 import dooyit.parser.*;
 import dooyit.logic.commands.*;
-import static org.junit.Assert.*;
+import org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+@RunWith(PowerMockRunner.class);
+@PrepareForTest(Parser.class);
 public class ParserTest {
 	private Parser parser = new Parser();
 	

4cc560be8cb1d2bc400bc2fbd35107a565972a78
Annabel Tay <annabel.tay17@gmail.com>
2016-03-19 17:09:29 +0800

    Completed MoveParser

---
+++ src/dooyit/parser/MoveParser.java
@@ -0,0 +1,84 @@
+package dooyit.parser;
+
+import dooyit.common.exception.IncorrectInputException;
+import dooyit.logic.commands.Command;
+import dooyit.logic.commands.CommandUtils;
+
+public class MoveParser extends TagParser {
+	private static final int INDEX_NAME = 0;
+	private static String catName;
+	private static String taskIds;
+	private static String userInput;
+	private static Command command;
+	
+	public MoveParser() {
+		super();
+	}
+	
+	public Command getCommand(String input) throws IncorrectInputException {
+		parse(input);
+		setVariables(taskIds);
+		switch (getTagType()) {
+		case SINGLE:
+			try {
+				parseSingleType();
+			} catch (IncorrectInputException e) {
+				command = getInvalidCommand(e.getMessage());
+				break;
+			}
+			command = getSingleTypeMoveCommand();
+			break;
+
+		case MULTIPLE:
+			try {
+				parseMultipleType();
+			} catch (IncorrectInputException e) {
+				command = getInvalidCommand(e.getMessage());
+				break;
+			}
+			command = getMultipleTypeMoveCommand();
+			break;
+
+		case INTERVAL:
+			try {
+				parseIntervalType();
+			} catch (IncorrectInputException e) {
+				command = getInvalidCommand(e.getMessage());
+				break;
+			}
+			command = getIntervalTypeMoveCommand();
+			break;
+
+		case INVALID:
+			command = getInvalidCmd();
+			break;
+		}
+		return command;
+	}
+
+	private Command getIntervalTypeMoveCommand() {
+		return CommandUtils.createMoveCommand(catName, taskIdsForTagging);
+	}
+
+	// Eg. delete 5 6 8
+	private Command getMultipleTypeMoveCommand() {
+		return CommandUtils.createMoveCommand(catName, taskIdsForTagging);
+	}
+
+	private Command getSingleTypeMoveCommand() {
+		return CommandUtils.createMoveCommand(catName, taskIdForTagging);
+	}
+
+	private Command getInvalidCmd() {
+		return CommandUtils.createInvalidCommand("Invalid Delete Command!");
+	}
+
+	private void parse(String input) {
+		command = null;
+		userInput = input;
+		String[] inputArr = userInput.split("//s+");
+		catName = inputArr[INDEX_NAME];
+		int indexTaskIds = userInput.indexOf(catName) + catName.length();
+		taskIds = userInput.substring(indexTaskIds);
+	}
+}

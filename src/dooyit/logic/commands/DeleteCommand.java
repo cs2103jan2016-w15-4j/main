657f6777adc7c86bac0c60d406a2f2220687783d
Ta Eu <taeu93@gmail.com>
2016-03-21 16:08:26 +0800

    combine single delete and batch delete

--- src/dooyit/logic/commands/DeleteCommand.java
+++ src/dooyit/logic/commands/DeleteCommand.java
@@ -7,60 +7,73 @@ import dooyit.logic.core.LogicController;
 import dooyit.logic.core.TaskManager;
 
 public class DeleteCommand extends Command {
-	public enum DeleteCommandType {
-		SINGLE, MULTIPLE
-	};
 
 	private int deleteId;
 	private ArrayList<Integer> deleteIds;
-	private DeleteCommandType deleteCommandType;
 
 	public DeleteCommand() {
-
+		deleteIds = new ArrayList<Integer>();
 	}
 
 	public void initDeleteCommand(int deleteId) {
-		deleteCommandType = DeleteCommandType.SINGLE;
-		this.deleteId = deleteId;
+		deleteIds.add(deleteId);
 	}
 
 	public void initDeleteCommand(ArrayList<Integer> deleteIds) {
-		deleteCommandType = DeleteCommandType.MULTIPLE;
-		this.deleteIds = deleteIds;
+		this.deleteIds.addAll(deleteIds);
 	}
 
 	@Override
 	public void execute(LogicController logic) throws IncorrectInputException {
 		TaskManager taskManager = logic.getTaskManager();
 		assert (taskManager != null);
+		
+		
+		String errorMessageBody = "";
 
-		switch (deleteCommandType) {
-
-		case SINGLE:
-			if (taskManager.containsTask(deleteId)) {
-				taskManager.deleteTask(deleteId);
+		for (int i = 0; i < deleteIds.size(); i++) {
+			if (taskManager.containsTask(deleteIds.get(i))) {
+				taskManager.deleteTask(deleteIds.get(i));
 			} else {
-				throw new IncorrectInputException("Task ID " + deleteId + " doesn't exists");
+				errorMessageBody += " " + deleteIds.get(i);
 			}
-			break;
-
-		case MULTIPLE:
-			String errorMessageBody = "";
-
-			for (int i = 0; i < deleteIds.size(); i++) {
-				if (taskManager.containsTask(deleteIds.get(i))) {
-					taskManager.deleteTask(deleteIds.get(i));
-				} else {
-					errorMessageBody += " " + deleteIds.get(i);
-				}
-			}
-
-			if (errorMessageBody != "") {
-				throw new IncorrectInputException("Index" + errorMessageBody + " doesn't exists");
-			}
-
-			break;
 		}
+
+		if (errorMessageBody != "") {
+			throw new IncorrectInputException("Index" + errorMessageBody + " doesn't exists");
+		}
+		
+		
+//		switch (deleteCommandType) {
+//
+//		case SINGLE:
+//			if (taskManager.containsTask(deleteId)) {
+//				taskManager.deleteTask(deleteId);
+//			} else {
+//				throw new IncorrectInputException("Task ID " + deleteId + " doesn't exists");
+//			}
+//			break;
+//
+//		case MULTIPLE:
+//			String errorMessageBody = "";
+//
+//			for (int i = 0; i < deleteIds.size(); i++) {
+//				if (taskManager.containsTask(deleteIds.get(i))) {
+//					taskManager.deleteTask(deleteIds.get(i));
+//				} else {
+//					errorMessageBody += " " + deleteIds.get(i);
+//				}
+//			}
+//
+//			if (errorMessageBody != "") {
+//				throw new IncorrectInputException("Index" + errorMessageBody + " doesn't exists");
+//			}
+//
+//			break;
+//		}
+		
+		
+		
 
 	}
 }

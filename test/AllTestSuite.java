19fe5cbc95475b0d0d4ae6d1a7ee46f19c0a18b4
Ta Eu <taeu93@gmail.com>
2016-03-23 00:35:18 +0800

    AllTestSuite

---
+++ test/AllTestSuite.java
@@ -0,0 +1,12 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import dooyit.logic.commands.CommandTestSuite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ CommandTestSuite.class})
+
+public class AllTestSuite {
+
+}

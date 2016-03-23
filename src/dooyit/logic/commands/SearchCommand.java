091c495238905858231faf0f745e28224cadef10
Ta Eu <taeu93@gmail.com>
2016-03-23 22:55:20 +0800

    added search command

---
+++ src/dooyit/logic/commands/SearchCommand.java
@@ -0,0 +1,13 @@
+package dooyit.logic.commands;
+
+import dooyit.common.exception.IncorrectInputException;
+import dooyit.logic.api.LogicController;
+
+public class SearchCommand extends Command {
+
+	@Override
+	public void execute(LogicController logic) throws IncorrectInputException {
+		
+	}
+
+}

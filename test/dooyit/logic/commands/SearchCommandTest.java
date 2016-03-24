bdf9a6f39c32fd69079d6d8e740499ba771bd10c
Ta Eu <taeu93@gmail.com>
2016-03-24 11:25:04 +0800

    search command test template

---
+++ test/dooyit/logic/commands/SearchCommandTest.java
@@ -0,0 +1,30 @@
+package dooyit.logic.commands;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import dooyit.common.datatype.DateTime;
+import dooyit.common.datatype.DeadlineTask;
+import dooyit.common.datatype.EventTask;
+import dooyit.common.datatype.FloatingTask;
+import dooyit.common.datatype.Task;
+import dooyit.logic.api.LogicController;
+import dooyit.logic.api.TaskManager;
+
+public class SearchCommandTest {
+	LogicController logicController;
+	SearchCommand addCommand;
+	
+	@Before
+	public void setUp(){
+		
+	}
+	
+	@Test
+	public void search(){
+		
+	}
+	
+}

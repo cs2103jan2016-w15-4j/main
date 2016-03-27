14491be471c4ee1819c8e105c9063bb6091a12db
Dexter Mooi <mooicydexter@gmail.com>
2016-03-27 13:20:48 +0800

    Added some JUnit test cases for AddParser. Added ParserTestSuite class for future use

---
+++ test/dooyit/parser/ParserTestSuite.java
@@ -0,0 +1,10 @@
+package dooyit.parser;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({AddParserTest.class})
+public class ParserTestSuite {
+
+}

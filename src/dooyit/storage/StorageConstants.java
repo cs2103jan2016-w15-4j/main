e72957c17599f6f89b49961889ea84a942283fb3
Dexter Mooi <mooicydexter@gmail.com>
2016-03-29 15:00:30 +0800

    Refactored StorageController and StorageConstants

--- src/dooyit/storage/StorageConstants.java
+++ src/dooyit/storage/StorageConstants.java
@@ -11,6 +11,7 @@ public class StorageConstants {
 	static final String FOLDER_DATA = CURRENT_DIRECTORY + SEPARATOR_CHAR + "data" + SEPARATOR_CHAR;
 
 	static final String DEFAULT_CATEGORIES_DESTINATION = FOLDER_DATA + "categories.txt";
+	static final String DEFAULT_BACKUP_DESTINATION = FOLDER_DATA + "backup.txt";
 	static final String DEFAULT_TASKS_DESTINATION = FOLDER_DATA + "tasks.txt";
 	static final String DEFAULT_THEME_DESTINATION = FOLDER_DATA + "custom.css";
 	

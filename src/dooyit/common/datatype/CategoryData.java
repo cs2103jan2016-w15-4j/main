252f4198770942600f668177dd74dde08f154558
Dexter Mooi <mooicydexter@gmail.com>
2016-03-29 20:06:27 +0800

    Changed constructor for CategoryData to public

--- src/dooyit/common/datatype/CategoryData.java
+++ src/dooyit/common/datatype/CategoryData.java
@@ -4,7 +4,7 @@ public class CategoryData {
 	private String name;
 	private String color;
 
-	CategoryData(String categoryName, String colorName) {
+	public CategoryData(String categoryName, String colorName) {
 		this.name = categoryName;
 		this.color = colorName;
 	}

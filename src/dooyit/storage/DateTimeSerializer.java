ee25a2ba572c123c4b99420f01977e7a8054f49d
Annabel Tay <annabel.tay17@gmail.com>
2016-03-29 17:26:03 +0800

    Revert "Revert "Updated DateTimeSerializer to only save date only""
    This reverts commit f908a734ca5b7c94b58257e43511457d47568e2b.
    
    Revert to where DateTimeSerializer to only save date only

--- src/dooyit/storage/DateTimeSerializer.java
+++ src/dooyit/storage/DateTimeSerializer.java
@@ -10,11 +10,14 @@ import com.google.gson.JsonSerializer;
 import dooyit.common.datatype.DateTime;
 
 public class DateTimeSerializer implements JsonSerializer<DateTime> {
+	
+	private static final String SPACE = " ";
 
 	@Override
 	public JsonElement serialize(DateTime dt, Type typeOfSrc, JsonSerializationContext context) {
 		JsonObject jsonObject = new JsonObject();
-		String date = "" + dt.getDD() + " " + dt.getMM() + " " + dt.getYY() + " " + dt.getDayStr();
+		String date = "";
+		date = dt.getDD() + SPACE + dt.getMM() + SPACE + dt.getYY();
 		jsonObject.addProperty("date", date);
 		jsonObject.addProperty("time", dt.getTimeInt());
 		return jsonObject;

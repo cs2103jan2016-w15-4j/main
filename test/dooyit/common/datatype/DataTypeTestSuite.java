e1103cf291cb440b3e5016dee2bcdfa5bd52f2c7
Dexter Mooi <mooicydexter@gmail.com>
2016-03-29 20:14:51 +0800

    Re-added DataTypeTestSuite

---
+++ test/dooyit/common/datatype/DataTypeTestSuite.java
@@ -0,0 +1,10 @@
+package dooyit.common.datatype;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({CategoryTest.class, DateTimeTest.class, EventTaskTest.class, FloatingTaskTest.class, TaskGroupTest.class, TaskTest.class})
+	public class DataTypeTestSuite {
+
+}

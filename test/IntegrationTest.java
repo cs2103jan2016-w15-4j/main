7bfb10107515177363719ca73d2bb7bfc6d8c55d
Dexter Mooi <mooicydexter@gmail.com>
2016-04-04 08:59:43 +0800

    Updated IntegrationTest to complement unique ID

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -3,11 +3,9 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-import dooyit.common.datatype.DateTime;
-import dooyit.common.datatype.DeadlineTask;
-import dooyit.common.datatype.EventTask;
-import dooyit.common.datatype.FloatingTask;
 import dooyit.common.datatype.Task;
+import dooyit.common.datatype.Task.TaskType;
+import dooyit.common.exception.IncorrectInputException;
 import dooyit.logic.api.LogicController;
 
 public class IntegrationTest {
@@ -23,40 +21,43 @@ public class IntegrationTest {
 	
 	
 	@Test
-	public void addFloatingTask(){
+	public void addFloatingTask_ValidInput_FloatingTask(){
 		logic.clearTask();
 		logic.processInput("add a");
 		
-		FloatingTask task = new FloatingTask("a");
+		Task task = logic.getTaskManager().getMostRecentTask();
+		
 		assertTrue(logic.containsTask(task));
+		assertEquals(TaskType.FLOATING, task.getTaskType());
 	}
 	
 	@Test
-	public void addDeadlineTask(){
+	public void addDeadlineTask_ValidDateTime_DeadlineTask(){
 		logic.clearTask();
-		logic.processInput("add buy milk by 30/3/2016");
+		logic.processInput("add buy milk by 30/3/2016");	
 		
-		int date[] = {30, 3, 2016};
-		DateTime dateTimeDeadline = new DateTime(date);
-		
-		DeadlineTask task = new DeadlineTask("buy milk", dateTimeDeadline);
+		Task task = logic.getTaskManager().getMostRecentTask();
 		assertTrue(logic.containsTask(task));
+		assertEquals(TaskType.DEADLINE, task.getTaskType());
 	}
 	
 	@Test
-	public void addEventTask(){
+	public void addEventTask_ValidDateTime_EventTask(){
 		logic.clearTask();
 		logic.processInput("add buy milk from 30/3/2016 to 31/3/2016");
 		
-		int dateStart[] = {30, 3, 2016};
-		int dateEnd[] = {31, 3, 2016};
-		DateTime dateTimeStart = new DateTime(dateStart);
-		DateTime dateTimeEnd = new DateTime(dateEnd);
-		
-		EventTask task = new EventTask("buy milk", dateTimeStart, dateTimeEnd);
+		Task task = logic.getTaskManager().getMostRecentTask();
 		assertTrue(logic.containsTask(task));
+		assertEquals(TaskType.EVENT, task.getTaskType());
 	}
 	
-	
+	@Test
+	public void addEventTask_InvalidDateTime_FloatingTask() {
+		logic.clearTask();
+		logic.processInput("add buy milk from 30/3/2016");
+		
+		Task task = logic.getTaskManager().getMostRecentTask();
+		assertEquals(TaskType.FLOATING, task.getTaskType());
+	}
 	
 }

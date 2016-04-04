8c1115226e7f0b660c30a749d774fa8ef8bc7329
Ta Eu <taeu93@gmail.com>
2016-04-05 01:10:30 +0800

    delete task feedback

--- src/dooyit/logic/commands/DeleteTaskCommand.java
+++ src/dooyit/logic/commands/DeleteTaskCommand.java
@@ -5,12 +5,15 @@ import java.util.ArrayList;
 
 import dooyit.common.datatype.Task;
 import dooyit.common.exception.IncorrectInputException;
+import dooyit.logic.Constants;
 import dooyit.logic.api.Action;
 import dooyit.logic.api.LogicAction;
 import dooyit.logic.api.LogicController;
 
 public class DeleteTaskCommand implements Command, ReversibleCommand {
 
+	private static final String FEEDBACK_TASKS_DELETED = "Tasks%1$s has been deleted.";
+	private static final String FEEDBACK_TASK_DELETED = "Task%1$s has been deleted.";
 	private ArrayList<Integer> deleteIds;
 	private ArrayList<Task> deletedTasks;
 	private boolean hasError = false;
@@ -48,22 +51,32 @@ public class DeleteTaskCommand implements Command, ReversibleCommand {
 		assert (logic != null);
 		LogicAction logicAction = null;
 
-		String errorMessageBody = "";
-
+		String errorMsgBody = Constants.EMPTY_STRING;
+		String deletedTaskMsg = Constants.EMPTY_STRING;
+		
+		
 		for (Integer deleteId : deleteIds) {
 			if (logic.containsTask(deleteId)) {
 				Task deletedTask = logic.removeTask(deleteId);
 				deletedTasks.add(deletedTask);
-
-				logicAction = new LogicAction(Action.DELETE_TASK);
+				deletedTaskMsg += " " + deleteId;
 			} else {
-				errorMessageBody += " " + "[" + deleteId + "]";
+				errorMsgBody += " " + deleteId;
 			}
 		}
-
-		if (errorMessageBody != "") {
-			logicAction = new LogicAction(Action.DELETE_TASK);
-			throw new IncorrectInputException("Index" + errorMessageBody + " doesn't exists");
+		
+		if(!deletedTasks.isEmpty()){
+			if(deletedTasks.size() == 1){
+				logicAction = new LogicAction(Action.DELETE_TASK, String.format(FEEDBACK_TASK_DELETED, deletedTaskMsg));
+			}else{
+				logicAction = new LogicAction(Action.DELETE_TASK, String.format(FEEDBACK_TASKS_DELETED, deletedTaskMsg));
+			}
+			
+		}else{
+			if (errorMsgBody != Constants.EMPTY_STRING) {
+				logicAction = new LogicAction(Action.ERROR, String.format("Index %1$s doesn't exists", errorMsgBody));
+				//throw new IncorrectInputException("Index" + errorMessageBody + " doesn't exists");
+			}
 		}
 
 		return logicAction;

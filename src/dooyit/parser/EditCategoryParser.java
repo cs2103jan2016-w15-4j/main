c1d04b3332f269d0e1684ff45924b55fe7e4524d
Annabel Tay <annabel.tay17@gmail.com>
2016-04-07 00:17:37 +0800

    Refactored EditCategoryParser class

--- src/dooyit/parser/EditCategoryParser.java
+++ src/dooyit/parser/EditCategoryParser.java
@@ -5,6 +5,9 @@ import dooyit.logic.commands.Command;
 import dooyit.logic.commands.CommandUtils;
 
 public class EditCategoryParser implements ParserCommons {
+	private static final String ERROR_MESSAGE_INVALID_EDIT_CATEGORY_COMMAND = "Invalid Edit Category Command!";
+	private static final String ERROR_MESSAGE_TOO_FEW_ARGUMENTS = "Too few arguments for Edit Category Command";
+	private static final String ERROR_MESSAGE_BE_SUCCINCT = "Can't you be more succinct in your category naming?";
 	private static final String MARKER_COLOUR = " to ";
 	private static final int INDEX_ORIGINAL_NAME = 0;
 	private static final int INDEX_NEW_NAME = 1;
@@ -36,27 +39,27 @@ public class EditCategoryParser implements ParserCommons {
 		switch(getEditType()) {
 		
 		case EDIT_NAME_AND_COLOUR: 
-			//command = CommandUtils.createEditCategoryCommand(originalName, newName, newColour);
+			command = CommandUtils.createEditCategoryCommand(originalName, newName, newColour);
 			break;
 		
 		case EDIT_NAME_ONLY:
-			//command = CommandUtils.creatEditCategoryNameCommand(originalName, newName);
+			command = CommandUtils.createEditCategoryCommand(originalName, newName);
 			break;
 		
 		case EDIT_COLOUR_ONLY:
-			//command = CommandUtils.createEditCategoryColourCommand(originalName, newColour);
+			//command = CommandUtils.createEditCategoryCommand(originalName, newColour);
 			break;
 		
 		case INVALID_TOO_MANY_WORDS: 
-			command = CommandUtils.createInvalidCommand("Can't you be more succinct in your category naming?");
+			command = CommandUtils.createInvalidCommand(ERROR_MESSAGE_BE_SUCCINCT);
 			break;
 		
 		case INVALID_TOO_FEW_ARGUMENTS:
-			command = CommandUtils.createInvalidCommand("Too few arguments for Edit Category Command");
+			command = CommandUtils.createInvalidCommand(ERROR_MESSAGE_TOO_FEW_ARGUMENTS);
 			break;
 		
 		default:
-			command = CommandUtils.createInvalidCommand("Invalid Edit Category Command!");
+			command = CommandUtils.createInvalidCommand(ERROR_MESSAGE_INVALID_EDIT_CATEGORY_COMMAND);
 			break;
 		}
 		return command;
@@ -66,14 +69,19 @@ public class EditCategoryParser implements ParserCommons {
 		int editType;
 		if(hasTooManyWordsInNewCategoryName) {
 			editType = INVALID_TOO_MANY_WORDS;
+			
 		} else if(hasInsufficientArguments) {
 			editType = INVALID_TOO_FEW_ARGUMENTS;
+			
 		} if(newName != null && newColour != null) {
 			editType = EDIT_NAME_AND_COLOUR;
+			
 		} else if(newName == null && newColour != null) {
 			editType = EDIT_COLOUR_ONLY;
+			
 		} else if(newName != null && newColour == null) {
 			editType = EDIT_NAME_ONLY;
+			
 		} else {
 			editType = EDIT_INVALID;
 		}
@@ -81,7 +89,7 @@ public class EditCategoryParser implements ParserCommons {
 	}
 
 	private void setVariables(String input) {
-		String[] inputArr = input.split("//s+");
+		String[] inputArr = input.split("\\s+");
 		originalName = inputArr[INDEX_ORIGINAL_NAME];
 		
 		if(inputArr.length == 1) {

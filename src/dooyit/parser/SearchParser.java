6725d8b68b24b5e89f3b3c46f83da25dedefb9dd
Annabel Tay <annabel.tay17@gmail.com>
2016-04-06 23:25:05 +0800

    Completed Search Parser:

--- src/dooyit/parser/SearchParser.java
+++ src/dooyit/parser/SearchParser.java
@@ -1,5 +1,48 @@
 package dooyit.parser;
 
-public class SearchParser {
+import dooyit.common.datatype.DateTime;
+import dooyit.common.datatype.DateTime.DAY;
+import dooyit.common.datatype.DateTime.MONTH;
+import dooyit.common.exception.IncorrectInputException;
+import dooyit.logic.commands.Command;
+import dooyit.logic.commands.CommandUtils;
 
+public class SearchParser implements ParserCommons {
+	
+	public SearchParser() {
+		
+	}
+	
+	public Command getCommand(String input) {
+		input = input.toLowerCase();
+		boolean isValidDate = true;
+		Command command = null;
+		DateTime dateTime;
+		
+		try {
+			DateTimeParser dtParser = new DateTimeParser();
+			dateTime = dtParser.parse(input);
+		} catch (IncorrectInputException e) {
+			isValidDate = false;
+		}
+		
+		DAY dayEnum = DateTime.getDayType(input);
+		MONTH monthEnum = DateTime.getMonthType(input);
+		boolean isValidDay = dayEnum != DAY.INVALID;
+		boolean isValidMonth = monthEnum != MONTH.INVALID;
+		boolean isEmptyString = input.equals(EMPTY_STRING);
+		
+		if(isEmptyString) {
+			command = CommandUtils.createInvalidCommand("Empty search command!");
+		} else if(isValidDay) {
+			//command = CommandUtils.createSearchCommand(input, dayEnum);
+		} else if(isValidMonth) {
+			//command = CommandUtils.createSearchCommand(input, monthEnum);
+		} else if(isValidDate) {
+			//command = CommandUtils.createSearchCommand(dateTime);
+		} else {
+			command = CommandUtils.createSearchCommand(input);
+		}
+		return command;
+	}
 }

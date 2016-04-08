2003b1ef954cf9a2ff3897f36fd12f077cf5a0b0
Dexter Mooi <mooicydexter@gmail.com>
2016-04-08 23:19:34 +0800

    Added more Parser checks in IntegrationTest as well as some comments

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.powermock.reflect.Whitebox;
 
+import dooyit.common.datatype.DateTime;
 import dooyit.common.datatype.Task;
 import dooyit.common.datatype.Task.TaskType;
 import dooyit.logic.api.LogicController;
@@ -30,14 +31,20 @@ public class IntegrationTest {
 		logic.clearTasks();
 		logic.processInput("add a");
 		
+		//Checking Logic
 		Task task = logic.getTaskManager().getMostRecentTask();
 		assertTrue(logic.containsTask(task));
 		assertEquals(TaskType.FLOATING, task.getTaskType());
+		
+		//Checking Parser
 		Parser parser = Whitebox.getInternalState(logic, "parser");
 		AddParser addParser = Whitebox.getInternalState(parser, "addParser");
+		
 		String userInput = Whitebox.getInternalState(addParser, "userInput");
 		assertEquals("a", userInput);
 		
+		String taskName = Whitebox.getInternalState(addParser, "taskName");
+		assertEquals("a", taskName);
 	}
 	
 	@Test
@@ -45,9 +52,24 @@ public class IntegrationTest {
 		logic.clearTasks();
 		logic.processInput("add buy milk by 30/3/2016");	
 		
+		//Checking Logic
 		Task task = logic.getTaskManager().getMostRecentTask();
 		assertTrue(logic.containsTask(task));
 		assertEquals(TaskType.DEADLINE, task.getTaskType());
+		
+		//Checking Parser
+		Parser parser = Whitebox.getInternalState(logic, "parser");
+		AddParser addParser = Whitebox.getInternalState(parser, "addParser");
+		
+		String userInput = Whitebox.getInternalState(addParser, "userInput");
+		assertEquals("buy milk by 30/3/2016", userInput);
+		
+		String taskName = Whitebox.getInternalState(addParser, "taskName");
+		assertEquals("buy milk", taskName);
+		
+		DateTime dt = Whitebox.getInternalState(addParser, "deadline");
+		DateTime expectedDateTime = new DateTime(new int[]{30, 3, 2016});
+		assertTrue(dt.equals(expectedDateTime));
 	}
 	
 	@Test
@@ -55,9 +77,28 @@ public class IntegrationTest {
 		logic.clearTasks();
 		logic.processInput("add buy milk from 30/3/2016 to 31/3/2016");
 		
+		//Checking Logic
 		Task task = logic.getTaskManager().getMostRecentTask();
 		assertTrue(logic.containsTask(task));
 		assertEquals(TaskType.EVENT, task.getTaskType());
+		
+		// Checking Parser
+		Parser parser = Whitebox.getInternalState(logic, "parser");
+		AddParser addParser = Whitebox.getInternalState(parser, "addParser");
+
+		String userInput = Whitebox.getInternalState(addParser, "userInput");
+		assertEquals("buy milk from 30/3/2016 to 31/3/2016", userInput);
+
+		String taskName = Whitebox.getInternalState(addParser, "taskName");
+		assertEquals("buy milk", taskName);
+
+		DateTime start = Whitebox.getInternalState(addParser, "start");
+		DateTime expectedStart = new DateTime(new int[] { 30, 3, 2016 }, 0);
+		assertTrue(start.equals(expectedStart));
+		
+		DateTime end = Whitebox.getInternalState(addParser, "end");
+		DateTime expectedEnd = new DateTime(new int[] { 31, 3, 2016 }, 2359);
+		assertTrue(end.equals(expectedEnd));
 	}
 	
 	@Test
@@ -65,8 +106,19 @@ public class IntegrationTest {
 		logic.clearTasks();
 		logic.processInput("add buy milk from 30/3/2016");
 		
+		//Checking Logic
 		Task task = logic.getTaskManager().getMostRecentTask();
 		assertEquals(TaskType.FLOATING, task.getTaskType());
+		
+		//Checking Parser
+		Parser parser = Whitebox.getInternalState(logic, "parser");
+		AddParser addParser = Whitebox.getInternalState(parser, "addParser");
+		
+		String userInput = Whitebox.getInternalState(addParser, "userInput");
+		assertEquals("buy milk from 30/3/2016", userInput);
+		
+		String taskName = Whitebox.getInternalState(addParser, "taskName");
+		assertEquals("buy milk from 30/3/2016", taskName);
 	}
 	
 }

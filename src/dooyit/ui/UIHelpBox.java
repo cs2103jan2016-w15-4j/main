8b76309ec868a8ccf77acd8af2308be0af44947c
Wenqi Wu <wenqiwu93@gmail.com>
2016-04-10 14:17:15 +0800

    updated help box

--- src/dooyit/ui/UIHelpBox.java
+++ src/dooyit/ui/UIHelpBox.java
@@ -19,8 +19,8 @@ import javafx.stage.Stage;
 
 public class UIHelpBox {
 
-	private static final int WIDTH = 500;
-	private static final int HEIGHT = 450;
+	private static final int WIDTH = 640;
+	private static final int HEIGHT = 480;
 	private static final String STYLECLASS_TITLE = UIStyle.HELP_BOX_TITLE;
 	private static final String LABEL_TITLE = "Hola! Here are some tips to get you started.";
 	private static final int LABEL_HEIGHT = 20;
@@ -28,19 +28,29 @@ public class UIHelpBox {
 	private static final int SPACING_CONTENT_WRAPPER = 80;
 	private static final int SPACING_CONTENT = 10;
 	private static final String STYLECLASS_HELP_BOX_WRAPPER = UIStyle.HELP_BOX_WRAPPER;
-	private static final int SPACING_HELP_BOX_WRAPPER = 60;
+	private static final int SPACING_HELP_BOX_WRAPPER = 40;
 	private static final String STYLECLASS_CLOSE_LABEL = "help-box-close-label";
 	private static final String CLOSE_LABEL_TITLE = "CLOSE";
-	private static final String DESC_ADD_TASK = "Add task";
-	private static final String DESC_MARK_TASK = "Mark task as complete";
-	private static final String DESC_DEL_TASK = "Delete task";
-	private static final String DESC_SHOW_VIEW = "Show different views";
-	private static final String DESC_QUIT_APP = "Quit Dooyit";
-	private static final String CMD_ADD_TASK = "add <task name>";
-	private static final String CMD_MARK_TASK = "mark <task index>";
-	private static final String CMD_DEL_TASK = "delete <task index>";
-	private static final String CMD_SHOW_VIEW = "show <view>";
-	private static final String CMD_QUIT_APP = "exit";
+	private static final String DESC_ADD = "add: create a task or event";
+	private static final String DESC_ADDCAT = "addcat: create a category";
+	private static final String DESC_CLEAR = "clear: delete ALL tasks";
+	private static final String DESC_DELETE = "delete: delete a task";
+	private static final String DESC_DELETECAT = "deletecat: delete a category";
+	private static final String DESC_EDIT = "edit: edit details of a task";
+	private static final String DESC_EDITCAT = "editcat: edit details of a category";
+	private static final String DESC_EXIT = "exit: quit the application";
+	private static final String DESC_FLOAT = "float: make a task floating";
+	private static final String DESC_MARK = "mark: mark a task as done";
+	private static final String DESC_MOVE = "move: move a task to a category";
+	private static final String DESC_REDO = "redo: redo an undone command";
+	private static final String DESC_REMOVE = "remove: take a task out of a category";
+	private static final String DESC_SHOW = "show: switch between views";
+	private static final String DESC_SHOWCAT = "showcat: switch to a category view";
+	private static final String DESC_SEARCH = "search: search for tasks and events";
+	private static final String DESC_SKIN = "skin: change application skin";
+	private static final String DESC_STORAGE = "storage: specify storage directory";
+	private static final String DESC_UNMARK = "unmark: unmark a completed task";
+	private static final String DESC_UNDO = "undo: undo the previous command";
 
 	private Popup helpBox;
 	private VBox helpBoxWrapper;
@@ -54,22 +64,32 @@ public class UIHelpBox {
 	private static final ArrayList<String> cmdNameList = new ArrayList<String>(){
 		private static final long serialVersionUID = 1L;
 		{
-			add(DESC_ADD_TASK);
-			add(DESC_MARK_TASK);
-			add(DESC_DEL_TASK);
-			add(DESC_SHOW_VIEW);
-			add(DESC_QUIT_APP);
+			add(DESC_ADD);
+			add(DESC_ADDCAT);
+			add(DESC_CLEAR);
+			add(DESC_DELETE);
+			add(DESC_DELETECAT);
+			add(DESC_EDIT);
+			add(DESC_EDITCAT);
+			add(DESC_EXIT);
+			add(DESC_FLOAT);
+			add(DESC_MARK);
 		}
 	};
 	
 	private static final ArrayList<String> cmdDescList = new ArrayList<String>(){
 		private static final long serialVersionUID = 1L;
 		{
-			add(CMD_ADD_TASK);
-			add(CMD_MARK_TASK);
-			add(CMD_DEL_TASK);
-			add(CMD_SHOW_VIEW);
-			add(CMD_QUIT_APP);
+			add(DESC_MOVE);
+			add(DESC_REDO);
+			add(DESC_REMOVE);
+			add(DESC_SHOW);
+			add(DESC_SHOWCAT);
+			add(DESC_SEARCH);
+			add(DESC_SKIN);
+			add(DESC_STORAGE);
+			add(DESC_UNMARK);
+			add(DESC_UNDO);
 		}
 	};
 

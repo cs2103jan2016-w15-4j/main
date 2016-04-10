9d94609d4edf7f4841224bcaf05c09a3f42092d9
Annabel Tay <annabel.tay17@gmail.com>
2016-04-10 19:03:20 +0800

    Refactored and edited comments for DeleteParser

--- src/dooyit/parser/DeleteParser.java
+++ src/dooyit/parser/DeleteParser.java
@@ -9,9 +9,9 @@ import dooyit.logic.commands.Command;
 import dooyit.logic.commands.CommandUtils;
 
 /**
- * The DeleteParser class provides methods needed for deleting tasks.
- * It takes in a "delete" or "remove" or "rm" command input and returns 
- * a DeleteCcommand object. It is a child class of the TagParser class.
+ * The DeleteParser class takes in a "delete" or "remove" or "rm" 
+ * command input and returns a DeleteCcommand object. It is needed
+ * for deleting tasks. It is a child class of the TagParser class.
  * 
  * @author Annabel
  *
@@ -19,9 +19,6 @@ import dooyit.logic.commands.CommandUtils;
 public class DeleteParser extends TagParser {
 	// Error message
 	private static final String ERROR_MESSAGE_INVALID_DELETE_COMMAND = "Invalid Delete Command!";
-	
-	// DeleteParser object attribute
-	private Command command;
 	
 	// Logger for DeleteParser
 	private static Logger logger = Logger.getLogger("DeleteParser");
@@ -33,58 +30,48 @@ public class DeleteParser extends TagParser {
 	}
 
 	/**
-	 * Parses userInput and returns the correct DeleteCommand object
+	 * Parses userInput and returns a DeleteCommand object
 	 * 
 	 * @param input
 	 * 		  The delete command input from the user
 	 * 
-	 * @return the correct DeleteCommand object if the taskIds are valid
-	 * 		   or the InvalidCommand object if the taskIds are invalid.
+	 * @return a DeleteCommand object if the taskIds are valid
+	 * 		   or an InvalidCommand object if the taskIds are invalid.
 	 */
 	public Command getCommand(String input) {
 		logger.log(Level.INFO, "Getting command object from DeleteParser");
-		// Reset object attribute for each call to the getCommand method
-		resetAttribute();
+		// Reset object attributes for each call to the getCommand method
 		setAttributesForTagging(input);
+		resetCommandAttributeToNull();
 		
+		setCommandAttribute();
+		return command;
+	}
+
+	/**
+	 * Sets command attribute to DeleteCommand or to InvalidCommand
+	 */
+	private void setCommandAttribute() {
 		// Checks if the taskIds are valid
 		try {
 			parseTaskIds();
 		} catch (IncorrectInputException e) {
-			setInvalidCommand(e.getMessage());
+			setToInvalidCommand(e.getMessage());
 		}
 		
 		// Checks if the command has been set to InvalidCommand object
 		if (command == null) {
-			setCorrectDeleteCommand();
+			// Set command attribute to a DeleteCommand object 
+			// if it hasn't been set set to an InvalidCommand object.
+			setToDeleteCommand();
 		}
-			
-		return command;
-	}
-	
-	/**
-	 * Resets the command attribute to null;
-	 */
-	private void resetAttribute() {
-		command = null;
-	}
-
-	/**
-	 * Sets the command attribute to an InvalidCommand object.
-	 * 
-	 * @param message
-	 * 		  Error message that indicates why the user input is wrong
-	 */
-	private void setInvalidCommand(String message) {
-		command = CommandUtils.createInvalidCommand(message);
 	}
 
 	/**
 	 * Sets command attribute to a DeleteCommand object or to
 	 * an InvalidCommand object.
-	 * 
 	 */
-	private void setCorrectDeleteCommand() {
+	private void setToDeleteCommand() {
 		switch (getTagType()) {
 		case VALID :
 			command = CommandUtils.createDeleteCommand(taskIdsForTagging);

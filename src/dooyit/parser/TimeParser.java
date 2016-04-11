e441ee19e2795e1ce67b29c6905367f64fb081b9
Annabel Tay <annabel.tay17@gmail.com>
2016-04-11 20:30:45 +0800

    Finished adding comments to TimeParser

--- src/dooyit/parser/TimeParser.java
+++ src/dooyit/parser/TimeParser.java
@@ -1,12 +1,15 @@
 //@@author A0133338J
 package dooyit.parser;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import dooyit.common.exception.IncorrectInputException;
 
 /**
- * 
- * It implements DateTimeparserCommons and ParserCommons
- * to use the shared methods and constants.
+ * TimeParser takes in a string and return the time in 24 hour integer format.
+ * It implements DateTimeparserCommons and ParserCommons to use the shared 
+ * methods and constants.
  * 
  * @author Annabel
  *
@@ -17,34 +20,56 @@ public class TimeParser implements DateTimeParserCommons, ParserCommons {
 	private static final String ERROR_MESSAGE_INVALID_HOURS_OR_MINUTES = "Invalid Time! Hours must fall within the 24h range and Minutes must be between 0 to 59 inclusive";
 	private static final String ERROR_MESSAGE_TIME_EXCEEDS_24H = "Invalid Time! Time must not exceed 24 hours!";
 	private static final String ERROR_MESSAGE_AM_AND_PM = "Time cannot be contain both am and pm!";
+	
+	// For checking if the time string is exactly midnight
 	private static final String TWELVE_MIDNIGHT_ONE_STRING = "12am";
+	
+	// For converting 12h afternoon timings like 2.30pm from 230 to 1430
 	private static final int TWELVE_HOURS = 1200;
+	
+	// For checking if 24h time int is equal to or greater than 24h
 	private static final int TWENTY_FOUR_HOURS = 2400;
+	
+	// Constants for the isMidnight method
 	private static final int FORMAT_24H_1259PM = 1259;
 	private static final int FORMAT_24H_12PM = 1200;
 	private static final int FORMAT_24H_1PM = 1300;
 	private static final int FORMAT_24H_1259AM = 59;
 	private static final int FORMAT_24H_12AM = 0;
+	private static final String FORMAT_DOUBLE_ZEROES = "00";
 	
+	// Constants for the accepted range of minutes
 	private static final int MINUTES_MINIMUM = 0;
 	private static final int MINUTES_MAXIMUM = 59;
+
+	// Logger for TimeParser
+	private static Logger logger = Logger.getLogger("TimeParser");
 	
-	/** */
+	/** Initializes a TimeParser object */
 	public TimeParser() {
-		
+		logger.log(Level.INFO, "Initialised TimeParser object");
 	}
 	
 	/**
+	 * Takes a valid time string from the user input and converts it into a time int.
 	 * 
-	 * @param splitInput
+	 * @param splitUserInput
+	 * 		  The user input String array.
+	 * 
 	 * @param combined
-	 * @param i
-	 * @return
-	 * @throws IncorrectInputException
+	 *        An int array that contains the day int, time int, date (DD,
+	 *        MM, YY) and the current index in the user input string array.
+	 *        
+	 * @param index
+	 *        The array index of the current word in the user input String array.
+	 *        
+	 * @return 24h time int of the valid time string in the user input.
+	 * 
+	 * @throws IncorrectInputException if the user input does not have a valid time string.
 	 */
-	public int[] parse(String[] splitInput, int[] combined, int i) throws IncorrectInputException {
+	public int[] parse(String[] splitUserInput, int[] combined, int index) throws IncorrectInputException {
 		try {
-			combined = getTimeFromUserInput(splitInput, i, combined);
+			combined = getTimeFromUserInput(splitUserInput, index, combined);
 		} catch (IncorrectInputException e) {
 			throw e;
 		}
@@ -52,13 +77,20 @@ public class TimeParser implements DateTimeParserCommons, ParserCommons {
 	}
 	
 	/**
+	 * Checks if the user input contains a valid time string.
 	 * 
 	 * @param currWord
-	 * @param userInput
+	 * 		  The word of the current word in the user input string array
+	 * 
+	 * @param splitUserInput
+	 * 		  The user input String array.
+	 * 
 	 * @param index
-	 * @return
+	 *        The array index of the current word in the user input String array.
+	 *        
+	 * @return true if user input has a valid time string and false otherwise.
 	 */
-	public boolean isValidTime(String currWord, String[] userInput, int index) {
+	public boolean isValidTime(String currWord, String[] splitUserInput, int index) {
 		boolean ans = false;
 		if (currWord.contains(PM) || currWord.contains(AM)) {
 			currWord = currWord.replace(PM, EMPTY_STRING).replace(AM, EMPTY_STRING);
@@ -77,8 +109,8 @@ public class TimeParser implements DateTimeParserCommons, ParserCommons {
 			}
 			
 		} else if (ParserCommons.isNumber(removeTimeSeparators(currWord))
-				&& DateTimeParserCommons.hasAWordAfterCurrWord(userInput, index)) {
-			String nextWord = userInput[index + 1];
+				&& DateTimeParserCommons.hasAWordAfterCurrWord(splitUserInput, index)) {
+			String nextWord = splitUserInput[index + 1];
 			if (nextWord.equals(AM) ^ nextWord.equals(PM)) {
 				ans = true;
 			}
@@ -89,24 +121,37 @@ public class TimeParser implements DateTimeParserCommons, ParserCommons {
 	}
 
 	/**
+	 * Removes the recognized time separators ":" and "." from the word input.
 	 * 
 	 * @param currWord
-	 * @return
+	 * 		  The word of the current word in the user input string array
+	 * 
+	 * @return the word input without the time separators.
 	 */
 	private String removeTimeSeparators(String currWord) {
 		return currWord.replace(TIME_SEPARATOR_COLON, EMPTY_STRING).replace(TIME_SEPARATOR_DOT, EMPTY_STRING);
 	}
 	
 	/**
+	 * Converts valid time string from user input into 24 time int and put the int into the 
+	 * combined int array.
 	 * 
-	 * @param splitInput
+	 * @param splitUserInput
+	 * 		  The user input String array.
+	 * 
 	 * @param index
+	 *        The array index of the current word in the user input String array.
+	 *        
 	 * @param combined
-	 * @return
-	 * @throws IncorrectInputException
+	 *        An int array that contains the day int, time int, date (DD,
+	 *        MM, YY) and the current index in the user input string array.
+	 *        
+	 * @return combined int array with the time value updated.
+	 * 
+	 * @throws IncorrectInputException if the user input does not contain a valid time string.
 	 */
-	private int[] getTimeFromUserInput(String[] splitInput, int index, int[] combined) throws IncorrectInputException {
-		String timeString = removeTimeSeparators(splitInput[index]);
+	private int[] getTimeFromUserInput(String[] splitUserInput, int index, int[] combined) throws IncorrectInputException {
+		String timeString = removeTimeSeparators(splitUserInput[index]);
 		boolean isAm = timeString.contains(AM);
 		boolean isPm = timeString.contains(PM);
 		int timeInt = UNINITIALIZED_INT;
@@ -123,50 +168,72 @@ public class TimeParser implements DateTimeParserCommons, ParserCommons {
 			timeInt = Integer.parseInt(timeString);
 		} 
 		
-		if (hasInvalidHoursOrMinutes(timeInt, splitInput, index, isAm, isPm)) {
+		if (hasInvalidHoursOrMinutes(timeInt, splitUserInput, index, isAm, isPm)) {
 			throw new IncorrectInputException(ERROR_MESSAGE_INVALID_HOURS_OR_MINUTES);
 		}
 
 		timeInt = getTimeIntByAssumingOneWordTimeInput(timeString, isAm, isPm, timeInt, isExactlyMidnight);
-		timeInt = getTimeIntByAssumingTwoWordsTimeInput(splitInput, index, timeInt);
+		timeInt = getTimeIntByAssumingTwoWordsTimeInput(splitUserInput, index, timeInt);
 		
 		if (timeExceeds24H(timeInt)) {
 			throw new IncorrectInputException(ERROR_MESSAGE_TIME_EXCEEDS_24H);
 		}
 
-		index = getNewIndex(splitInput, index);
+		index = getNewArrayIndex(splitUserInput, index);
 		combined = updateTimeAndIndexInCombinedArray(index, combined, timeInt);
 		
 		return combined;
 	}
 	
 	/**
+	 * Checks if the time int has invalid hour and/or minute value.
 	 * 
 	 * @param timeInt
-	 * @param splitInput
+	 * 		  Can be 12h (eg. 1100) or 24h (2300) format time integers.
+	 * 
+	 * @param splitUserInput
+	 * 		  The user input String array.
+	 * 
 	 * @param index
+	 *        The array index of the current word in the user input String array.
+	 *        
 	 * @param isAm
+	 * 		  Boolean input that indicates if the time String contains the "am" time marker.
+	 * 
 	 * @param isPm
-	 * @return
+	 * 		  Boolean input that indicates if the time String contains the "pm" time marker.
+	 * 
+	 * @return true if the timeInt has invalid hour and/or minute value and false otherwise.
 	 */
-	private boolean hasInvalidHoursOrMinutes(int timeInt, String[] splitInput, int index, boolean isAm, boolean isPm) {
-		return hasInvalidHours(timeInt, splitInput, index, isAm, isPm) || hasInvalidMinutes(timeInt);
+	private boolean hasInvalidHoursOrMinutes(int timeInt, String[] splitUserInput, int index, boolean isAm, boolean isPm) {
+		return hasInvalidHours(timeInt, splitUserInput, index, isAm, isPm) || hasInvalidMinutes(timeInt);
 	}
 	
 	/**
+	 * Checks if the time int has invalid hour value like 100 or 25.
 	 * 
 	 * @param timeInt
-	 * @param splitInput
+	 * 		  Can be 12h (eg. 1100) or 24h (2300) format time integers.
+	 * 
+	 * @param splitUserInput
+	 * 		  The user input String array.
+	 * 
 	 * @param index
+	 *        The array index of the current word in the user input String array.
+	 *        
 	 * @param isAm
+	 * 		  Boolean input that indicates if the time String contains the "am" time marker.
+	 * 
 	 * @param isPm
-	 * @return
+	 * 		  Boolean input that indicates if the time String contains the "pm" time marker.
+	 * 
+	 * @return true if the timeInt has invalid hour value and false otherwise.
 	 */
-	private boolean hasInvalidHours(int timeInt, String[] splitInput, int index, boolean isAm, boolean isPm) {
+	private boolean hasInvalidHours(int timeInt, String[] splitUserInput, int index, boolean isAm, boolean isPm) {
 		String nextWord;
 		boolean isAmOrPm = false, ans = false;
-		if (DateTimeParserCommons.hasAWordAfterCurrWord(splitInput, index)) {
-			nextWord = splitInput[index + 1];
+		if (DateTimeParserCommons.hasAWordAfterCurrWord(splitUserInput, index)) {
+			nextWord = splitUserInput[index + 1];
 			isAmOrPm = nextWord.contains(PM) || nextWord.contains(AM);
 		}
 		
@@ -179,18 +246,25 @@ public class TimeParser implements DateTimeParserCommons, ParserCommons {
 	}
 
 	/**
+	 * Checks if hour value exceeds or is equals to 24.
 	 * 
 	 * @param timeInt
-	 * @return
+	 * 		  Time string converted to 24h format int, For eg. 12.00am is 0 and 11.59pm is 2359.
+	 * 
+	 * @return true if the hour is equal to or greater than 24 and false otherwise.
 	 */
 	private boolean timeExceeds24H(int timeInt) {
 		return timeInt >= TWENTY_FOUR_HOURS;
 	}
 
 	/**
+	 * Check if the minute value of the time int is within the acceptable range for 
+	 * minutes.
 	 * 
 	 * @param timeInt
-	 * @return
+	 * 		  Time string converted to 24h format int, For eg. 12.00am is 0 and 11.59pm is 2359.
+	 * 
+	 * @return true if the minute value is within the range and false otherwise.
 	 */
 	private boolean hasInvalidMinutes(int timeInt) {
 		int minutes = getMinuteNumeralOfTime(timeInt);
@@ -198,11 +272,18 @@ public class TimeParser implements DateTimeParserCommons, ParserCommons {
 	}
 
 	/**
+	 * Removes "am" and "pm" marker from the timeString.
 	 * 
 	 * @param timeString
+	 *   	  The time string from the user input.
+	 *   
 	 * @param isAm
+	 * 		  Boolean input that indicates if the time String contained the "am" time marker.
+	 * 
 	 * @param isPm
-	 * @return
+	 * 		  Boolean input that indicates if the time String contained the "pm" time marker.
+	 * 
+	 * @return the current word without the "am" and "pm" time string.
 	 */
 	private String removeAmAndPmFromTimeString(String timeString, boolean isAm, boolean isPm) {
 		if (isAm ^ isPm) {
@@ -213,14 +294,20 @@ public class TimeParser implements DateTimeParserCommons, ParserCommons {
 	}
 	
 	/**
+	 * Checks if there is a need to increase the array index by 1. If the next word 
+	 * if am or pm, the array index will be incremented, else it won't be increased.
 	 * 
-	 * @param splitInput
+	 * @param splitUserInput
+	 * 		  The user input String array.
+	 * 
 	 * @param index
-	 * @return
+	 *        The array index of the current word in the user input String array.
+	 *        
+	 * @return the correct array index.
 	 */
-	private int getNewIndex(String[] splitInput, int index) {
-		if (DateTimeParserCommons.hasAWordAfterCurrWord(splitInput, index)) {
-			String nextWord = splitInput[index + 1];
+	private int getNewArrayIndex(String[] splitUserInput, int index) {
+		if (DateTimeParserCommons.hasAWordAfterCurrWord(splitUserInput, index)) {
+			String nextWord = splitUserInput[index + 1];
 			if (nextWord.equals(PM) || nextWord.equals(AM)) {
 				index += 1; // To skip the next word
 			}
@@ -229,16 +316,24 @@ public class TimeParser implements DateTimeParserCommons, ParserCommons {
 	}
 	
 	/**
+	 * Converts time string from the user into 24h time int. This is for time string of the
+	 * format "12.20 pm" or "8:15 am". Note the spacing before the time markers "am" and "pm"
 	 * 
-	 * @param splitInput
+	 * @param splitUserInput
+	 * 		  The user input String array.
+	 * 
 	 * @param index
+	 *        The array index of the current word in the user input String array.
+	 *        
 	 * @param timeInt
-	 * @return
+	 * 		  Time int in 12h format, 12pm and 12am are both 1200
+	 * 
+	 * @return 24h time int
 	 */
-	private int getTimeIntByAssumingTwoWordsTimeInput(String[] splitInput, int index, int timeInt) {
+	private int getTimeIntByAssumingTwoWordsTimeInput(String[] splitUserInput, int index, int timeInt) {
 		index += 1;
-		if (index < splitInput.length) {
-			String indicator = splitInput[index];
+		if (index < splitUserInput.length) {
+			String indicator = splitUserInput[index];
 			if (indicator.equals(PM) && !timeIsBetween1200And1259Inclusive(timeInt)) {
 				timeInt += TWELVE_HOURS;
 			}
@@ -250,13 +345,25 @@ public class TimeParser implements DateTimeParserCommons, ParserCommons {
 	}
 	
 	/**
+	 * Converts time string from the user into 24h time int. This is for time string of the
+	 * format "12.20pm" or "8:15am".
 	 * 
 	 * @param timeString
+	 *   	  The time string from the user input.
+	 *   
 	 * @param isAm
+	 * 		  Boolean input that indicates if the time String contains the "am" time marker.
+	 * 
 	 * @param isPm
+	 * 		  Boolean input that indicates if the time String contains the "pm" time marker.
+	 * 
 	 * @param timeInt
+	 * 		  Can be either 12h or 24 time format.
+	 * 
 	 * @param isExactlyMidnight
-	 * @return
+	 *        Boolean input that indicates if the time string from the user input is exactly 12 midnight.
+	 *        
+	 * @return 24h format time int.
 	 */
 	private int getTimeIntByAssumingOneWordTimeInput(String timeString, boolean isAm, boolean isPm, int timeInt, boolean isExactlyMidnight) {
 		if (isMidnight(timeInt, timeString, isAm) || isExactlyMidnight) {
@@ -265,12 +372,13 @@ public class TimeParser implements DateTimeParserCommons, ParserCommons {
 			} else {
 				timeInt = getMinuteNumeralOfTime(timeInt);
 			}
-		} else if (is12HFormatTime(timeInt)) { // this is for the 12h format
-			timeInt = convertTimeFrom12HTo24HFormat(timeInt);
+		} else if (is12HNoMinutesFormatTime(timeInt)) { // Eg 12am or 1pm
+			timeInt = convertTimeHourFrom12HTo24HFormat(timeInt);
 		}
 
 		if (isPm) {
 			if(!timeIsBetween1200And1259Inclusive(timeInt)) {
+				// Convert time int from eg 100 to 1300 for 1pm
 				timeInt = timeInt + TWELVE_HOURS;
 			}
 		}
@@ -278,38 +386,56 @@ public class TimeParser implements DateTimeParserCommons, ParserCommons {
 	}
 
 	/**
+	 * Checks if the time int is between 12pm and 11.59pm.
 	 * 
 	 * @param timeInt
-	 * @return
+	 * 		  Can be in 12h or 24h time format.
+	 * 
+	 * @return true if time int is within the stated range and false otherwise.
 	 */
 	private boolean timeIsBetween1200And1259Inclusive(int timeInt) {
 		return timeInt >= FORMAT_24H_12PM && timeInt < FORMAT_24H_1PM;
 	}
 	
 	/**
+	 * Converts hour value time int from 12h format to 24h format. 
+	 * Eg. "1" for 1pm and 1am is converted to "100" format.
 	 * 
 	 * @param timeInt
-	 * @return
+	 * 		  Hour value of time int in 12h format.
+	 * 
+	 * @return hour value in 24h format
 	 */
-	private int convertTimeFrom12HTo24HFormat(int timeInt) {
+	private int convertTimeHourFrom12HTo24HFormat(int timeInt) {
 		return timeInt * 100;
 	}
 
 	/**
+	 * Checks if the time int is of the format 12am or 1pm or 2pm, eg no minutes.
 	 * 
 	 * @param timeInt
-	 * @return
+	 * 		  Hour value of time int converted from time string.
+	 * 
+	 * @return true if it the stated format and false otherwise.
 	 */
-	private boolean is12HFormatTime(int timeInt) {
+	private boolean is12HNoMinutesFormatTime(int timeInt) {
 		return timeInt <= 12;
 	}
 	
 	/**
+	 * Updates the time and counter fields in the combined int array.
 	 * 
 	 * @param index
+	 *        The array index of the current word in the user input String array.
+	 * 
 	 * @param combined
+	 *        An int array that contains the day int, time int, date (DD,
+	 *        MM, YY) and the current index in the user input string array.
+	 *        
 	 * @param timeInt
-	 * @return
+	 * 		  Time string in 24h format int, For eg. 12.00am is 0 and 11.59pm is 2359.
+	 * 
+	 * @return combined int array with time and counter fields updated.
 	 */
 	private int[] updateTimeAndIndexInCombinedArray(int index, int[] combined, int timeInt) {
 		combined[COMBINED_INDEX_TIME] = timeInt;
@@ -318,23 +444,33 @@ public class TimeParser implements DateTimeParserCommons, ParserCommons {
 	}
 	
 	/**
+	 * Gets the minute numeral from the 24h time int.
 	 * 
 	 * @param timeInt
-	 * @return
+	 * 		  Time string converted 24h format int, For eg. 12.00am is 0 and 11.59pm is 2359.
+	 * 
+	 * @return the minute numeral of the time int.
 	 */
 	private int getMinuteNumeralOfTime(int timeInt) {
 		return timeInt % 100;
 	}
 
 	/**
+	 * Checks if the time string is after 12am and before 1am 
 	 * 
 	 * @param timeInt
+	 * 		  Time string in 24h format int, For eg. 12.00am is 0 and 11.59pm is 2359.
+	 * 
 	 * @param timeString
+	 *   	  The time string from the user input.
+	 *   
 	 * @param isAm
-	 * @return
+	 * 		  Boolean input that indicates if the time String contained the "am" time marker
+	 * 
+	 * @return true if the timeString is between 12.00am to 12.59am and false otherwise.
 	 */
 	private boolean isMidnight(int timeInt, String timeString, boolean isAm) {
-		boolean isMidnight24H = timeInt >= FORMAT_24H_12AM && timeInt <= FORMAT_24H_1259AM && timeString.contains("00");
+		boolean isMidnight24H = timeInt >= FORMAT_24H_12AM && timeInt <= FORMAT_24H_1259AM && timeString.contains(FORMAT_DOUBLE_ZEROES);
 		boolean isMidnight12H = timeInt >= FORMAT_24H_12PM && timeInt <= FORMAT_24H_1259PM && isAm;
 		return isMidnight24H || isMidnight12H;
 	}
